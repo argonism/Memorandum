873dde40ab92c242c5387d21e1fd9a91798d9536
k-usuha <kouta528bado@yahoo.co.jp>
2020-08-29 23:25:48 +0900

    update: create-post.sh

--- create-post.sh
+++ create-post.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 # create article with title
 DATE=`date '+%Y-%m-%-d'`
+YEAR=`date '+%Y'`
 TITLE=$1
 if [ $# -lt 1 ]; then
   echo 'usage: {this program} ${title}'
@@ -12,7 +13,7 @@ FORMAT="+%Y-%m-%-d--${TITLE}"
 FILENAME=`date ${FORMAT}`
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-ARTICLE_PATH="${SCRIPT_DIR}/content/blog/${FILENAME}.md"
+ARTICLE_PATH="${SCRIPT_DIR}/content/blog/${YEAR}/${FILENAME}.md"
 
 if [ -f $ARTICLE_PATH ]; then
   echo "${FILENAME} already existed."
