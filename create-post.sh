0ac40d7f72984185bed2478a15a68a2fb7488d34
k-usuha <kouta528bado@yahoo.co.jp>
2020-09-25 20:46:22 +0900

    update: tags feature to create-post

--- create-post.sh
+++ create-post.sh
@@ -2,30 +2,46 @@
 #!/bin/bash
 # create article with title
 
+if [ $# -lt 1 ]; then
+  echo 'usage: sh create-post.sh {title} {category} {tag}'
+  echo "title is required.\n"
+  exit 1
+fi
+
 ShapeTitle () {
     echo $1 | sed 's/[\. ]/-/g'
 }
 
 DATE=`date '+%Y-%m-%-d'`
 YEAR=`date '+%Y'`
-if [ $# -lt 1 ]; then
-  echo 'usage: sh create-post.sh {title} {category} {tag}'
-  echo "title is required.\n"
-  exit 1
-fi
 TITLE=`ShapeTitle "$1"`
 
+# set Category
 CATEGORY=$2
 if [ -z "$CATEGORY" ]; then
-  echo "Diary"
+  CATEGORY="Diary"
 fi
 
+# set Tags
+if [ $# -lt 3 ]; then
+    TAGS="\n    - \"new\""
+fi
+
+for x in "${@:3:($#)}"
+do
+    TAG="\n    - \"${x}\""
+    TAGS=$TAGS$TAG
+done
+
+# set filename
 FORMAT="+%Y-%m-%-d--${TITLE}"
 FILENAME=`date ${FORMAT}`
 
+# set generate target path
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ARTICLE_PATH="${SCRIPT_DIR}/content/blog/${YEAR}/${FILENAME}.md"
 
+# avoid overwrite
 if [ -f $ARTICLE_PATH ]; then
   echo "${FILENAME} already existed."
   exit 1
@@ -36,8 +52,7 @@ title: ${TITLE}
 date: '${DATE}'
 draft: false
 category: '${CATEGORY}'
-tags:
-  - 'new'
+tags: ${TAGS}
 cover: '../media/'
 ---" > $ARTICLE_PATH
 
