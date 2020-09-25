0ee9e08ef694798118454e17b36f9fa9d70487a6
k-usuha <kouta528bado@yahoo.co.jp>
2020-09-25 19:38:36 +0900

    update: create-post title

--- create-post.sh
+++ create-post.sh
@@ -2,14 +2,18 @@
 #!/bin/bash
 # create article with title
 
+ShapeTitle () {
+    echo $1 | sed 's/[\. ]/-/g'
+}
+
 DATE=`date '+%Y-%m-%-d'`
 YEAR=`date '+%Y'`
-TITLE=$1
 if [ $# -lt 1 ]; then
-  echo 'usage: {this program} {title} {category} {tag}'
+  echo 'usage: sh create-post.sh {title} {category} {tag}'
   echo "title is required to create article.\n"
   exit 1
 fi
+TITLE=`ShapeTitle "$1"`
 FORMAT="+%Y-%m-%-d--${TITLE}"
 FILENAME=`date ${FORMAT}`
 
