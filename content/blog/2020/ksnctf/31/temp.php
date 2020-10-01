1f6278f0e55c3a12ef0fbe1ce405e759f75e938c
k-usuha <kouta528bado@yahoo.co.jp>
2020-10-02 02:20:29 +0900

    add: article ksnctf q.31

---
+++ content/blog/2020/ksnctf/31/temp.php
@@ -0,0 +1,84 @@
+<?php
+
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
+
+
+$salt = 'FLAG_????????????????';
+
+$shipname = array(
+    'Nagato',
+    'Mutsu',
+    'Kongo',
+    'Hiei',
+    'Haruna',
+    'Kirishima',
+    'Fuso',
+    'Yamashiro',
+    'Ise',
+    'Hyuga',
+    "Yamato [Congratulations! The flag is $salt. ??????????????????????????????????????.]"
+);
+
+//  Check signature and read
+if (isset($_COOKIE['ship']) and
+    isset($_COOKIE['signature']) and
+    hash('sha512', $salt.$_COOKIE['ship']) === $_COOKIE['signature']) {
+      $ship = explode(',', $_COOKIE['ship']);
+  }
+else {
+  $ship = array();
+}
+
+if (isset($_POST['submit']))
+{
+  print($_COOKIE['ship']);
+    //  Gacha
+    if ($_POST['submit'] === 'Gacha')
+    {
+        // console_log($_COOKIE['ship']);
+        // console_log(hash('sha512', $salt.$_COOKIE['ship']));
+        //  Yamato is ultra rare
+        $ship[] = mt_rand(0, count($shipname)-2);
+
+        $s = implode(',', $ship);
+        $sign = hash('sha512', $salt.$s);
+
+        setcookie('ship', $s);
+        setcookie('signature', $sign);
+    }
+
+    //  Clear
+    if ($_POST['submit'] === 'Clear')
+    {
+        setcookie('ship', '', 0);
+        setcookie('signature', '', 0);
+    }
+
+    header("Location: {$_SERVER['REQUEST_URI']}");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>KanGacha</title>
+  </head>
+  <body>
+    <h1>KanGacha</h1>
+    <form method="POST">
+      <input type="submit" name="submit" value="Gacha">
+      <input type="submit" name="submit" value="Clear">
+    </form>
+    <ul>
+<?php
+for ($i=0; $i<count($ship); $i++)
+    echo "<li>{$shipname[$ship[$i]]}</li>\n";
+?>
+    </ol>
+  </body>
+</html>
